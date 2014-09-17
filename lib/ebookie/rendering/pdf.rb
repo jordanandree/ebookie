f4ad9bc95a6e5947b1874528b43e46638e6cd2b9
jordanandree <jordan.andree@mailchimp.com>
2014-09-17 16:48:41 -0400

    cleanup PDF renderer

--- lib/ebookie/rendering/pdf.rb
+++ lib/ebookie/rendering/pdf.rb
@@ -11,6 +11,57 @@ module Ebookie
       set :paths, %w(images)
       set :images_dir, 'images'
 
+      def after_initialize
+        @tmp_path = tmpdir.join("document.pdf")
+        @pages = []
+      end
+
+      def process!
+        convert_page
+
+        ::PDF::Reader.new(@tmp_path).pages.each_with_index do |page, idx|
+          prune_blank_page(idx)
+        end
+
+        Prawn::Document.generate(output, pdf_options) do |pdf|
+          @pages.each do |idx|
+            pdf.start_new_page( template: tmpdir.join("page-#{idx}.pdf") )
+          end
+        end
+      end
+
+      def sanitize(content)
+        content.gsub! /src=('|")([a-zA-Z0-9\.]+)('|")/, 'src=\1' + tmpdir.to_s + '/images/\2\3'
+        content
+      end
+
+      def convert_page
+        script = File.expand_path("../html2pdf.js", __FILE__)
+        system [Phantomjs.path, script, tmpdir.join('document.html'), @tmp_path].join(' ')
+      end
+
+      def prune_blank_page(index)
+        ghostscript_path = `which gs`.chomp
+        raise "GhostScript not installed" unless ghostscript_path
+
+        output_file = tmpdir.join("page-#{index}.pdf")
+        options = ["-q",
+                   "-dBATCH",
+                   "-dNOPAUSE",
+                   "-sDEVICE=pdfwrite",
+                   "-dFirstPage=#{index+1}",
+                   "-dLastPage=#{index+1}",
+                   "-sOutputFile=#{output_file}"]
+
+        system [ghostscript_path, options.join(' '), @tmp_path].join(' ')
+
+        colors = `#{ghostscript_path} -q -o - -sDEVICE=inkcov #{output_file}`
+        colors = colors.split(" ").uniq[0..-3]
+        unless colors.length == 1 && colors.first == "0.00000"
+          @pages << index.to_s
+        end
+      end
+
       def pdf_options
         {
           :info => {
@@ -23,45 +74,6 @@ module Ebookie
             :CreationDate => document.date
           }
         }
-      end
-
-      def process!
-        ghostscript_path = `which gs`.chomp
-        phantom_script = File.expand_path("../html2pdf.js", __FILE__)
-
-        tmp_path = tmpdir.join("document.pdf")
-        system "#{Phantomjs.path} #{phantom_script} '#{tmpdir.join('document.html')}' #{tmp_path}"
-        pages = []
-
-        reader = ::PDF::Reader.new(tmp_path)
-
-        reader.pages.each_with_index do |page, idx|
-          output_file = tmpdir.join("page-#{idx}.pdf")
-
-          gs_opts = "-q -dBATCH -dNOPAUSE \
-                    -sDEVICE=pdfwrite \
-                    -dFirstPage=#{idx+1} \
-                    -dLastPage=#{idx+1} \
-                    -sOutputFile=#{output_file}"
-          system("#{ghostscript_path} #{gs_opts} #{tmp_path}")
-
-          colors = `#{ghostscript_path} -q -o - -sDEVICE=inkcov #{output_file}`
-          colors = colors.split(" ").uniq[0..-3]
-          unless colors.length == 1 && colors.first == "0.00000"
-            pages << idx.to_s
-          end
-        end
-
-        Prawn::Document.generate(output, pdf_options) do |pdf|
-          pages.each do |idx|
-            pdf.start_new_page( template: tmpdir.join("page-#{idx}.pdf") )
-          end
-        end
-      end
-
-      def sanitize(content)
-        content.gsub! /src=('|")([a-zA-Z0-9\.]+)('|")/, 'src=\1' + tmpdir.to_s + '/images/\2\3'
-        content
       end
 
     end
